6fbc1c26e08c24582eacb07b1d57ced55ded2b74
fawda123 <mbafs2012@gmail.com>
2023-02-13 07:44:46 -0500

    added otb segment coverage ani to sob 2022

--- R/seagrass_segment_coverage.R
+++ R/seagrass_segment_coverage.R
@@ -3,6 +3,9 @@ library(sf)
 library(here)
 library(tbeptools)
 library(magick)
+library(gridExtra)
+
+# segment coverages by year -------------------------------------------------------------------
 
 fls <- list.files(path = 'T:/04_STAFF/MARCUS/03_GIT/hmpu-workflow/data', pattern = 'sgdat', full.names = T)
 
@@ -29,7 +32,6 @@ segswfwmd <- st_read('T:/05_GIS/SWFWMD/Seagrass/2022_Seagrass/provisional/DraftM
     waterbodyName = factor(waterbodyName, levels = levs, labels = labs)
   ) %>% 
   select(segment = waterbodyName)
-
 
 sgyrs <- fls %>% 
   basename %>% 
@@ -162,11 +164,11 @@ for(yr in sgdat$yr){
     filter(Year <= maxyr)
   
   p2 <- ggplot2::ggplot(na.omit(toplo2), ggplot2::aes(x = ind, y = Acres)) +
-    ggplot2::geom_col(fill = '#00806E', colour = 'black', width = 1.3) +
+    ggplot2::geom_col(fill = '#00806E', colour = 'black', width = 1.4) +
     ggplot2::geom_segment(x = 0, xend = 32, y = 38, yend = 38, col = 'red', size = 2) +
     ggplot2::geom_segment(x = 32, xend = nrow(toplo) + 1, y = 40, yend = 40, col = 'red', size = 2) +
     # ggplot2::annotate("text", label = "Seagrass Coverage Goal", x = 4, y = 40.5, color = 'red', size = 5, hjust = 0) +
-    ggplot2::annotate('text', x = brks[which(lbs == maxyr)], y = lasty, label = lastlab, angle = 90, hjust = 1, vjust = 0.3, size = 3) +
+    ggplot2::annotate('text', x = brks[which(lbs == maxyr)], y = lasty, label = lastlab, angle = 90, hjust = 1, vjust = 0.3, size = 3, color = 'white') +
     ggplot2::scale_x_continuous(breaks = brks, labels = lbs2, limits = c(0, max(brks) + 1), expand = c(0.01, 0.01)) +
     ggplot2::scale_y_continuous(expand = c(0, 0), limits = c(0, 1.1 * max(toplo$Acres, na.rm = T))) +
     ggplot2::theme_grey() +
@@ -196,3 +198,111 @@ list.files(path = here('figure'), pattern = '^sgdat', full.names = TRUE) %>%
   image_join() %>% # joins image
   image_animate(fps=1) %>% # animates, can opt for number of loops
   image_write(here('figure/sgcovani.gif')) 
+
+# coverage animation Old Tampa Bay ------------------------------------------------------------
+
+load(file = here('data/sgsegest.RData'))
+load(file = here('data/sgdat.RData'))
+
+otbsegshed <- tbsegshed %>% 
+  filter(bay_segment == 'OTB') %>% 
+  st_transform(crs = st_crs(sgdat))
+
+otbsgdat <- sgdat %>% 
+  st_intersection(otbsegshed)
+
+otbsgsegest <- sgsegest %>%
+  filter(segment == 'Old Tampa Bay') %>% 
+  select(
+    Year = year, 
+    Acres = acres
+    )
+
+toplo <- tibble::tibble(
+  Year = seq(1988, 2022)
+) %>%
+  dplyr::left_join(otbsgsegest, by = 'Year') %>%
+  dplyr::mutate(
+    Acres = Acres / 1000,
+    ind = 1:nrow(.)
+  )
+
+# axis labels
+lbs <- c(toplo$Year)
+brks <- c(toplo$ind)
+lbs2 <- lbs
+lbs2[as.numeric(lbs) %% 2 != 0] <- ''
+
+frm <- st_bbox(otbsgdat) %>% 
+  st_as_sfc()
+
+for(yr in otbsgdat$yr){
+  
+  cat(yr, '\n')
+  
+  toplocov <- otbsgdat %>% 
+    filter(yr == !!yr)
+  
+  p1 <- ggplot() + 
+    geom_sf(data = frm, fill = NA, color = NA) +
+    geom_sf(data = toplocov, fill = 'darkgreen', color = 'darkgreen') + 
+    theme_void() +  
+    theme(
+      plot.margin = margin(0,0,0,0, 'cm')
+    ) + 
+    labs(title = yr)
+  
+  maxyr <- yr
+  
+  # label for last bar
+  lastlab <- otbsgsegest %>%
+    filter(Year == maxyr) %>%
+    pull(Acres) %>%
+    round(0) %>%
+    format(big.mark = ',') %>%
+    paste(., 'acres')
+  
+  # y loc for last bar label
+  lasty <- otbsgsegest %>%
+    filter(Year == maxyr) %>%
+    pull(Acres) %>%
+    `/`(1000) %>%
+    `-`(0.1)
+  
+  toplo2 <- toplo %>% 
+    filter(Year <= maxyr)
+  
+  p2 <- ggplot2::ggplot(na.omit(toplo2), ggplot2::aes(x = ind, y = Acres)) +
+    ggplot2::geom_col(fill = '#00806E', colour = 'black', width = 1.4) +
+    ggplot2::geom_segment(x = 0, xend = nrow(toplo) + 1, y = 11.1, yend = 11.1, col = 'red', size = 2) +
+    # ggplot2::annotate("text", label = "Seagrass Coverage Goal", x = 4, y = 40.5, color = 'red', size = 5, hjust = 0) +
+    ggplot2::annotate('text', x = brks[which(lbs == maxyr)], y = lasty, label = lastlab, angle = 90, hjust = 1, vjust = 0.3, size = 3, color = 'white') +
+    ggplot2::scale_x_continuous(breaks = brks, labels = lbs2, limits = c(0, max(brks) + 1), expand = c(0.01, 0.01)) +
+    ggplot2::scale_y_continuous(expand = c(0, 0), limits = c(0, 1.1 * max(toplo$Acres, na.rm = T))) +
+    ggplot2::theme_grey() +
+    ggplot2::theme(
+      axis.line = ggplot2::element_line(),
+      panel.background = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 10),
+      axis.title.x = ggplot2::element_blank(),
+      legend.position = 'none',
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major = ggplot2::element_blank()
+    ) +
+    labs(
+      y = 'Seagrass Coverage (x1,000 acres)'
+    )  
+  
+  flnm <- paste0('figure/otbsgdat', yr, '.png')
+  
+  png(flnm, height = 3.5, width = 8, units = 'in', res = 200)
+  grid.arrange(p1, p2, ncol = 2, widths = c(1, 1))
+  dev.off()
+  
+}
+
+list.files(path = here('figure'), pattern = '^otbsgdat', full.names = TRUE) %>% 
+  image_read() %>% # reads each path file
+  image_join() %>% # joins image
+  image_animate(fps=1) %>% # animates, can opt for number of loops
+  image_write(here('figure/otbsgcovani.gif')) 
